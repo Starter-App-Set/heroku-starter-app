03ebcb4011f45e11e60de6f6a587ede7de5afc7a
Kaushik Chaubal <kaushikchaubal@gmail.com>
2016-10-08 20:41:45 +0100

    Adding CORs

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var gzippo = require('gzippo');
 var express = require('express');
+var cors = require('cors');
 var data = require('./data.json');
 
 var app = express();
 app.use(gzippo.staticGzip("" + __dirname));
+app.use(cors());
 
 var port = process.env.PORT || 5000;
 app.listen(port);
