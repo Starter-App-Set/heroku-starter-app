61e9884f14f4e4263bc9095e66fb599bf24b4c2b
Kaushik Chaubal <kaushikchaubal@gmail.com>
2016-10-08 18:47:23 +0100

    Removing cors dependency

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
-var cors = require('cors');
 var data = require('./data.json');
 
 var app = express();
-app.use(cors());
 
 var port = process.env.PORT || 5000;
 app.listen(port);
