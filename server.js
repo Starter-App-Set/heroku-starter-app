f637e6fcf2ac68b65db1b2a1de80fd43e44a5c4e
Kaushik Chaubal <kaushikchaubal@gmail.com>
2016-10-08 19:29:50 +0100

    Updating JSON

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+var gzippo = require('gzippo');
 var express = require('express');
 var data = require('./data.json');
 
 var app = express();
+app.use(gzippo.staticGzip("" + __dirname));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
